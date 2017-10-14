4251051ca2bd23457b943aede37ae305b762ae50
Alex Hendren <alex@oa-sys.io>
2017-10-14 13:59:48 -0500

    Create Query Test project

---
+++ examples/query/include/query.h
@@ -0,0 +1,29 @@
+/* query.h
+ *
+ * This is the main package file. Include this file in other projects.
+ * Only modify inside the header-end and body-end sections.
+ */
+
+#ifndef QUERY_H
+#define QUERY_H
+
+#include <corto/corto.h>
+#include <corto/corto.h>
+#include <query/_project.h>
+#include <corto/c/c.h>
+
+/* $header() */
+/* Definitions that are required by package headers (native types) go here. */
+/* $end */
+
+#include <query/_type.h>
+#include <query/_interface.h>
+#include <query/_load.h>
+#include <query/c/_api.h>
+
+/* $body() */
+/* Definitions here that need your package headers go here. */
+/* $end */
+
+#endif
+
---
+++ examples/query/src/query.c
@@ -0,0 +1,11 @@
+/* This is a managed file. Do not delete this comment. */
+
+#include <query/query.h>
+
+int queryMain(int argc, char *argv[]) {
+
+    /* Insert implementation */
+    
+    return 0;
+}
+
