aed8388802500b13627884d3539a1ab137862e9e
Alex Hendren <alex@oa-sys.io>
2017-10-15 20:50:04 -0500

    Initialize mount test project

---
+++ mnt/influxdb/test/src/HistoricalQuery.c
@@ -0,0 +1,10 @@
+/* This is a managed file. Do not delete this comment. */
+
+#include <include/test.h>
+
+void test_HistoricalQuery_select(
+    test_HistoricalQuery this)
+{
+    /* Insert implementation */
+}
+
---
+++ mnt/influxdb/test/src/Query.c
@@ -0,0 +1,10 @@
+/* This is a managed file. Do not delete this comment. */
+
+#include <include/test.h>
+
+void test_Query_resolve(
+    test_Query this)
+{
+    /* Insert implementation */
+}
+
