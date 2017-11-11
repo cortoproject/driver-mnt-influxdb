a760a52990c9c12889842378c0b0f7b90aff6170
Alex Hendren <alex@oa-sys.io>
2017-11-10 19:57:17 -0600

    Cleanup time query

--- src/query_builder.c
+++ src/query_builder.c
@@ -169,64 +169,33 @@ corto_string influxdb_Mount_query_builder_time(
 {
     corto_buffer buffer = CORTO_BUFFER_INIT;
 
-    // corto_string timeLimit = NULL;
-    // corto_string sampleLimit = NULL;
-    // corto_string depthLimit = NULL;
-    // if (query->timeBegin.kind == CORTO_FRAME_NOW) {
-    //     if (query->timeBegin.kind == CORTO_FRAME_TIME) {
-    //         corto_time t = corto_frame_getTime(&query->timeBegin);
-    //         timeLimit = corto_asprintf(" WHERE time > %ds", t.sec);
-    //     } else if (query->timeEnd.kind == CORTO_FRAME_DURATION) {
-    //         corto_time t = corto_frame_getTime(&query->timeEnd);
-    //         timeLimit = corto_asprintf(" WHERE time > (now() - %ds)", t.sec);
-    //     } else if (qu       ery->timeEnd.kind == CORTO_FRAME_SAMPLE) {
-    //         if (query->timeEnd.value) {
-    //             sampleLimit = corto_asprintf(" OFFSET %ld", query->timeEnd.value);
-    //         }
-    //
-    //     } else if (query->timeEnd.kind == CORTO_FRAME_DEPTH) {
-    //         depthLimit = corto_asprintf(" ORDER BY time DESC LIMIT %ld", query->timeEnd.value);
-    //     }
-    // } else if (query->timeBegin.kind == CORTO_FRAME_TIME) {
-    //     corto_time from = corto_frame_getTime(&query->timeBegin);
-    //     if (query->timeEnd.kind == CORTO_FRAME_TIME) {
-    //         corto_time to = corto_frame_getTime(&query->timeEnd);
-    //         timeLimit = corto_asprintf(" WHERE time < %ds AND time > %ds", from.sec, to.sec);
-    //     } else if (query->timeEnd.kind == CORTO_FRAME_DURATION) {
-    //         corto_time to = corto_frame_getTime(&query->timeEnd);
-    //         timeLimit = corto_asprintf(" WHERE time < %ds AND time > %ds",
-    //             from.sec,
-    //             from.sec - to.sec);
-    //     } else if (query->timeEnd.kind == CORTO_FRAME_SAMPLE) {
-    //         timeLimit = corto_asprintf(" WHERE time < %ds", from.sec);
-    //         if (query->timeEnd.value) {
-    //             sampleLimit = corto_asprintf(" OFFSET %ld", query->timeEnd.value);
-    //         }
-    //     } else if (query->timeEnd.kind == CORTO_FRAME_DEPTH) {
-    //         timeLimit = corto_asprintf(" WHERE time < %ds", from.sec);
-    //         depthLimit = corto_asprintf(" ORDER BY time DESC LIMIT %ld", query->timeEnd.value);
-    //     }
-    // } else if (query->timeBegin.kind == CORTO_FRAME_SAMPLE) {
-    //     if (query->timeEnd.kind == CORTO_FRAME_TIME) {
-    //         corto_time to = corto_frame_getTime(&query->timeEnd);
-    //         timeLimit = corto_asprintf(" WHERE time < %ds", to.sec);
-    //     }
-    // }
-    //
-    // if (timeLimit) {
-    //     corto_buffer_appendstr(&buffer, timeLimit);
-    //     corto_dealloc(timeLimit);
-    // }
-    //
-    // if (sampleLimit) {
-    //     corto_buffer_appendstr(&buffer, sampleLimit);
-    //     corto_dealloc(sampleLimit);
-    // }
-    //
-    // if (depthLimit) {
-    //     corto_buffer_appendstr(&buffer, depthLimit);
-    //     corto_dealloc(depthLimit);
-    // }
+    corto_string timeLimit = NULL;
+
+    if (query->timeBegin.kind == CORTO_FRAME_NOW) {
+        if (query->timeBegin.kind == CORTO_FRAME_TIME) {
+            corto_time t = corto_frame_getTime(&query->timeBegin);
+            timeLimit = corto_asprintf(" WHERE time > %ds", t.sec);
+        } else if (query->timeEnd.kind == CORTO_FRAME_DURATION) {
+            corto_time t = corto_frame_getTime(&query->timeEnd);
+            timeLimit = corto_asprintf(" WHERE time > (now() - %ds)", t.sec);
+        }
+    } else if (query->timeBegin.kind == CORTO_FRAME_TIME) {
+        corto_time from = corto_frame_getTime(&query->timeBegin);
+        if (query->timeEnd.kind == CORTO_FRAME_TIME) {
+            corto_time to = corto_frame_getTime(&query->timeEnd);
+            timeLimit = corto_asprintf(" WHERE time < %ds AND time > %ds",
+                from.sec, to.sec);
+        } else if (query->timeEnd.kind == CORTO_FRAME_DURATION) {
+            corto_time to = corto_frame_getTime(&query->timeEnd);
+            timeLimit = corto_asprintf(" WHERE time < %ds AND time > %ds",
+                from.sec, from.sec - to.sec);
+        }
+    }
+
+    if (timeLimit) {
+        corto_buffer_appendstr(&buffer, timeLimit);
+        corto_dealloc(timeLimit);
+    }
 
     return corto_buffer_str(&buffer);
 }
