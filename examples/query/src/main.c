aa314f2e774d0b62d2b04b09d72680b14edfadaf
Alex Hendren <alex@oa-sys.io>
2018-12-06 19:50:27 -0600

    Update query example to single app

--- examples/query/src/main.c
+++ examples/query/src/main.c
@@ -1,10 +1,12 @@
 /* This is a managed file. Do not delete this comment. */
 
-#include <query/query.h>
+#include <include/query.h>
+
+
 
 int cortomain(int argc, char *argv[]) {
 
     /* Insert implementation */
-
+    
     return 0;
 }
